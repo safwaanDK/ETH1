6383c81dab44a60e32552e7e9951ed26fd5d8f21
Sk Md Safwaan Uddin <101254419+safwaanDK@users.noreply.github.com>
2023-07-09 17:01:36 +0530

    Add files via upload

---
+++ excepHandle.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ExceptionHandling {
+    function divide(uint256 dividend, uint256 divisor) external pure returns (uint256) {
+        require(divisor != 0, "Division by zero is not allowed");
+        
+        uint256 quotient = dividend / divisor;
+        
+        assert(quotient * divisor == dividend);
+        
+        return quotient;
+    }
+
+    event AgeRequirementMet(address indexed account, uint256 age);
+
+    function checkAge(uint256 age) external {
+        require(age >= 18, "You must be at least 18 years old");
+        emit AgeRequirementMet(msg.sender, age);
+    }
+    
+    function withdraw(uint256 amount) external {
+        require(amount > 0, "Amount must be greater than zero");
+        require(address(this).balance >= amount, "Insufficient balance");
+    
+        // Perform the withdrawal
+        payable(msg.sender).transfer(amount);
+        
+    }
+}
